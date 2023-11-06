01cda53f3ca708f2bffb0f1acebf08014f122c1d
Tamás Vince Kornél <vin@kys.social>
2023-11-06 13:26:48 +0100

    hehe

--- src/lib/component/nav/UserBar.svelte
+++ src/lib/component/nav/UserBar.svelte
@@ -8,16 +8,22 @@
 
 <div class="flex flex-row justify-between mx-2">
   {#if $currentUser}
+  <div class="w-1/3">
     <p>{username}</p>
+  </div>
+  <div class="w-1/3 text-center">
     <button
-      on:click={() => {
-        goto('/post');
-      }}
+    on:click={() => {
+      goto('/post');
+    }}
       class="font-bold"
-    >
+      >
       new post</button
-    >
+      >
+  </div>
+  <div class="w-1/3 text-right">
     <button on:click={signOut}>sign out</button>
+  </div>
   {:else}
     <p />
     <button on:click={signIn}>sign in</button>
