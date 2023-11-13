954822b8db55d883d30a50bb952cfd6277906d56
Tamás Vince Kornél <tamas.vince.kornel@petrik.hu>
2023-11-13 21:10:12 +0100

    yes i added tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,11 @@
   />
   <meta content="PetrikMeme" property="og:site_name" />
   <meta content="https://petrikmeme.hu/favicon.png" property="og:image" />
+  <script
+    async
+    src="https://umami.vinn.fun/script.js"
+    data-website-id="4ec397df-8ec9-49e5-adf2-cfb2cc77d965"
+  ></script>
 </head>
 
 <div class="bg-slate-950">
