aae0f775876f5e43fa266ae06c7318a97628ee39
Tamás Vince Kornél <tamas.vince.kornel@petrik.hu>
2023-11-13 20:44:36 +0100

    it can only get better

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
   import '$lib/style/app.css';
 </script>
 
+<head>
+  <meta content="PetrikMeme - 100% ótvar képek" property="og:title" />
+  <meta
+    content="Gondolkoztál már rajta milyen lenne, ha lenne egy rendes Petrikes mém oldal? Mi se!"
+    property="og:description"
+  />
+  <meta content="PetrikMeme" property="og:site_name" />
+  <meta content="https://petrikmeme.hu/favicon.png" property="og:image" />
+</head>
+
 <div class="bg-slate-950">
   <slot />
 </div>
