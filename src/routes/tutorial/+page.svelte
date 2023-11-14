597f4d23995b76f89c12a58e9e02090f7fa0c07d
Tamás Vince Kornél <vin@kys.social>
2023-11-14 09:34:33 +0100

    update tutorial

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -8,7 +8,7 @@
 </head>
 
 <main
-  class="bg-gray-950 flex flex-row pt-1 max-h-screen min-h-screen text-white max-w-5xl mx-auto md:border"
+  class="bg-gray-950 flex flex-row pt-1 max-h-screen min-h-screen text-white max-w-3xl mx-auto md:border"
 >
   <div class="flex flex-col w-full">
     <Title />
@@ -35,13 +35,21 @@
           post.
         </p>
         <p>upload your image, give your post a title and press submit.</p>
-        <h2 class="text-xl mt-2">Posting <span class="font-bold">rules</span></h2>
+        <h2 class="text-xl mt-2"><span class="font-bold">Rules</span></h2>
         <ul class="list-disc ml-4">
           <li>
             posts must have a title, with a length of <span class="font-bold">3-30</span> characters
           </li>
           <li>
-            posts must include a <span class="font-bold">non NSFW</span> funny image, related to Petrik
+            posts must include an image <span class="font-bold"
+              >that does not break Hungarian law, and is not NSFW</span
+            >, related to Petrik
+          </li>
+          <li>
+            posts must be <a
+              href="https://dictionary.cambridge.org/dictionary/english/funny"
+              class="underline hover:font-bold">funny</a
+            >
           </li>
           <li>
             posts must be <a
@@ -52,14 +60,22 @@
           <li>post spamming will result in a ban.</li>
         </ul>
       </div>
-      <div class="mt-6 w-full px-2">
-        <button
-          class="text-center border p-1 text-xl h-10 bg-green-900 mb-3"
-          on:click={() => {
-            goto('/');
-          }}>i am ready.</button
-        >
+      <div class="mx-2">
+        <hr class="my-2" />
+        <h2 class="text-xl">Reporting</h2>
+        <p>
+          if you see a post that you think should not be there or is using material that is not
+          theirs, you can email us at
+          <a href="mailto:dmca@petrikmeme.hu" class="text-green-500"> dmca@petrikmeme.hu </a>
+          with the post's share link and the reason why you think it should be removed.
+        </p>
       </div>
+      <button
+        class="text-center border p-1 mt-8 text-xl h-10 bg-green-900 md:w-48 md:mx-auto mb-3"
+        on:click={() => {
+          goto('/');
+        }}>back to home</button
+      >
     </div>
   </div>
 </main>
